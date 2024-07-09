3a914ca96b7f88c6833cb2fb9e4794ad0f06dc2c
Mazzinii <luizeduardomazzini@gmail.com>
2024-07-09 20:43:23 -0300

    sign in function

---
+++ conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+    //configurando a conexao com o banco de dados     
+    $hostname = 'localhost';
+    $bancodedados = 'usuarios'; // nome do banco de dados já criado 
+    $usuario = 'root';
+    $senha = '';
+
+    //fazendo conexão
+    $mysqli = new mysqli($hostname,$usuario,$senha,$bancodedados); 
+
+    //tratando os erros 
+    if ($mysqli->connect_errno) {
+        echo "Falha ao conectar ao banco de dados: " . $mysqli->connect_error;
+    }
+    else 
+        echo "Conectado ao Banco de Dados";
+    
+
+
+
+?>
